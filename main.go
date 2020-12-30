b0b43aaf8bf7b40bdec2952312480f5b52efe2f4
Rodion <diakovrodion@mail.ru>
2020-12-30 21:44:10 +0300

    go Third Project

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+import(
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+	"html/template"
+)
+type Article struct {
+	Id uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+var showPost = Article{}
+func index(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("templates/index.html","templates/header.html","templates/footer.html",) 
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+	}
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		panic(err)
+	}
+	posts = []Article{}
+	for res.Next(){
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		posts = append(posts, post)
+	}
+
+	// fmt.Println("Подключено к MySQL")
+	t.ExecuteTemplate(w, "index", nil)
+}
+func create(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("templates/create.html","templates/header.html","templates/footer.html",) 
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+func save_article(w http.ResponseWriter, r *http.Request){
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+	if title == "" ||  anons == "" ||  full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	}else{
+
+		
+		db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES(`%s`, `%s`, `%s`)", title, anons, full_text))
+
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+		//http.Redirect(w, r, "/", 301)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func show_post(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html","templates/header.html","templates/footer.html",) 
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	// w.WriteHeader(http.StatusOk)
+	// fmt.Fprintf(w, "ID: %v\n", vars["id"])
+
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `articles` WHERE `id` == '%s'"),vars["id"])
+	if err != nil {
+		panic(err)
+	}
+	showPost = Article{}
+	for res.Next(){
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		// posts = append(posts, post)
+		showPost = post
+	}
+
+	// fmt.Println("Подключено к MySQL")
+	t.ExecuteTemplate(w, "show", showPost)
+}
+
+func handleFunc(){
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+	// http.HandleFunc("/", index)
+	// http.HandleFunc("/create", create)
+	// http.HandleFunc("/save_article", save_article)
+
+	http.Handle("/",rtr)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	
+	http.ListenAndServe(":8080", nil)
+}
+
+func main(){
+	handleFunc()
+}
